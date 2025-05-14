27280fe7ac36a22c6d74e47e36e44854a59efcd8
tejashxv <tejashyadavcr7@gmail.com>
2025-05-14 23:24:26 +0530

    typecasting example

--- first_tutorial/Demo.java
+++ first_tutorial/Demo.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 
  public class Demo {
     public static void main(String[] args) {
-        System.out.println("enter your input");
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine());
+        int a = 'z';
+        System.out.println(a);
 
 
     }
---
+++ first_tutorial/ImplicitCasting.java
@@ -0,0 +1,11 @@
+package first_tutorial;
+
+public class ImplicitCasting {
+    public static void main(String[] args) {
+        int intVal = 10;
+        double doubleVal = intVal;  // int to double
+
+        System.out.println("int value: " + intVal);
+        System.out.println("double value (after implicit casting): " + doubleVal);
+    }
+}
