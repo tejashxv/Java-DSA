60e0f7ab869e151a194cc9b18e460aa14d21758c
tejashxv <tejashyadavcr7@gmail.com>
2025-06-03 22:34:25 +0530

    main.java

--- function_methods/Main.java
+++ function_methods/Main.java
@@ -4,32 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Sum();
-        int a =Sum1();
-        System.out.println("Return value is: " + a);
-    }
-
-   static int Sum1(){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter num1: ");
-        int num1 = in.nextInt();
-        System.out.print("Enter Num2: ");
-        int num2 = in.nextInt();
-        int sum = num1 + num2;
-        System.out.println("Sum is: " + sum);
-        return sum;
-    }
+        int num1, num2, sum;
 
-    static void Sum(){
-        Scanner in = new Scanner(System.in);
+
+
         System.out.print("Enter num1: ");
-        int num1 = in.nextInt();
+        num1 = in.nextInt();
         System.out.print("Enter Num2: ");
-        int num2 = in.nextInt();
-        int sum = num1 + num2;
+        num2 = in.nextInt();
+        sum = num1 + num2;
         System.out.println("Sum is: " + sum);
 
-    }
+
 
 
 
