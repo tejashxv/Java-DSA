463067f34b0dd462cfbb07f6779d99601c8d0c8f
tejashxv <tejashyadavcr7@gmail.com>
2025-06-07 01:59:25 +0530

    .

--- function_methods/Main.java
+++ function_methods/Main.java
@@ -3,28 +3,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        int num1, num2, sum;
-
-
-
-        System.out.print("Enter num1: ");
-        num1 = in.nextInt();
-        System.out.print("Enter Num2: ");
-        num2 = in.nextInt();
-        sum = num1 + num2;
-        System.out.println("Sum is: " + sum);
-System.out.print("Enter num1: ");
-        num1 = in.nextInt();
-        System.out.print("Enter Num2: ");
-        num2 = in.nextInt();
-        sum = num1 + num2;
-        System.out.println("Sum is: " + sum);
-
-
-
-
+    Sum();
+    }
 
     /*
 
@@ -34,5 +14,17 @@ System.out.print("Enter num1: ");
       }
 
      */
+
+       static void Sum(){
+            Scanner in = new Scanner(System.in);
+            int num1, num2, sum;
+            System.out.print("Enter num1: ");
+            num1 = in.nextInt();
+            System.out.print("Enter Num2: ");
+            num2 = in.nextInt();
+            sum = num1 + num2;
+            System.out.println("Sum is: " + sum);
+
+        }
 
 }
